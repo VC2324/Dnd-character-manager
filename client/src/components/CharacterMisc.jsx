15be7dd932daecf0ccb69ca218228f1bb488e9e1
Victor <victorac2324@gmail.com>
2024-06-18 18:25:35 -0400

    added skills

--- client/src/components/CharacterMisc.jsx
+++ client/src/components/CharacterMisc.jsx
@@ -1,8 +1,53 @@
+// import React from 'react';
+
+// function CharacterMisc({ handleSubmit, profData, handleChange }) {
+//   return (
+//     <div className="flex">
+//       <form onSubmit={handleSubmit} className="ml-4">
+//         <div>
+//           <div className="flex flex-col gap-2"> {/* Flex container for stacked layout */}
+//             {/* Inspiration */}
+//             <div className="flex items-center">
+//               <input
+//                 type="number"
+//                 name="inspiration"
+//                 value={profData.misc_stats.inspiration}
+//                 onChange={handleChange}
+//                 // className="w-16 p-2 rounded shadow-sm outline-none text-center"
+//                 className="w-16 p-2 rounded-full border-2 border-black outline-none text-center"
+//                 required
+//               />
+//               {/* <label className="ml-2">Inspiration</label> */}
+//               {/* <label className="ml-2 border border-black p-1 rounded">Inspiration</label> */}
+//               <label className=" ml-2 p-2 border-2 border-black rounded font-dragon  outline-double ">Inspiration</label>
+//             </div>
+
+//             {/* Proficiency Bonus */}
+//             <div className="flex items-center">
+//               <input
+//                 type="number"
+//                 name="prof_bonus"
+//                 value={profData.misc_stats.prof_bonus}
+//                 onChange={handleChange}
+//                 // className="w-16 p-2 rounded shadow-sm outline-none text-center"
+//                 className="w-16 p-2 rounded-full border-2 border-black outline-none text-center"
+//                 required
+//               />
+//               <label className="ml-2 p-2 border-2 border-black rounded font-dragon  outline-double">Proficiency Bonus</label>
+//             </div>
+//           </div>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default CharacterMisc;
 import React from 'react';
 
 function CharacterMisc({ handleSubmit, profData, handleChange }) {
   return (
-    <div className="flex">
+    <div className="w-1/4 p-4">
       <form onSubmit={handleSubmit} className="ml-4">
         <div>
           <div className="flex flex-col gap-2"> {/* Flex container for stacked layout */}
@@ -13,13 +58,10 @@ function CharacterMisc({ handleSubmit, profData, handleChange }) {
                 name="inspiration"
                 value={profData.misc_stats.inspiration}
                 onChange={handleChange}
-                // className="w-16 p-2 rounded shadow-sm outline-none text-center"
                 className="w-16 p-2 rounded-full border-2 border-black outline-none text-center"
                 required
               />
-              {/* <label className="ml-2">Inspiration</label> */}
-              {/* <label className="ml-2 border border-black p-1 rounded">Inspiration</label> */}
-              <label className=" ml-2 p-2 border-2 border-black rounded font-dragon  outline-double ">Inspiration</label>
+              <label className="ml-2 p-2 border-2 border-black rounded font-dragon outline-double">Inspiration</label>
             </div>
 
             {/* Proficiency Bonus */}
@@ -29,11 +71,10 @@ function CharacterMisc({ handleSubmit, profData, handleChange }) {
                 name="prof_bonus"
                 value={profData.misc_stats.prof_bonus}
                 onChange={handleChange}
-                // className="w-16 p-2 rounded shadow-sm outline-none text-center"
                 className="w-16 p-2 rounded-full border-2 border-black outline-none text-center"
                 required
               />
-              <label className="ml-2 p-2 border-2 border-black rounded font-dragon  outline-double">Proficiency Bonus</label>
+              <label className="ml-2 p-2 border-2 border-black rounded font-dragon outline-double">Proficiency Bonus</label>
             </div>
           </div>
         </div>
--- client/src/components/CharacterSaving.jsx
+++ client/src/components/CharacterSaving.jsx
@@ -2,139 +2,145 @@ import React from 'react';
 
 function CharacterSaving({ handleSubmit, savingThrows, handleChange }) {
     return (
-        <div>
+        <div className="p-4 w-1/4 mx-auto">
+            <label className='text-xl underline font-dragon'>Saving Throws</label>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>
-                        Strength:
-                        <input
-                            type="number"
-                            name="saving_throws.strength"
-                            value={savingThrows.strength}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="saving_throws.strength_pro"
-                            checked={savingThrows.strength_pro}
-                            onChange={handleChange}
-                        />
-                        Proficient
-                    </label>
+                <div className="flex items-center mb-4">
+                    <input
+                        type="checkbox"
+                        name="saving_throws.strength_pro"
+                        checked={savingThrows.strength_pro}
+                        onChange={handleChange}
+                        // className="mr-2"
+                        //  className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-blue-600 checked:border-transparent focus:ring-0 focus:outline-none"
+                        // className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-gray-400 checked:border-transparent focus:ring-0 focus:outline-none"
+                        className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-red-400 checked:border-black focus:ring-5 focus:outline-none transition-all duration-300"
+                   />
+
+                    <input
+                        type="number"
+                        name="saving_throws.strength"
+                        value={savingThrows.strength}
+                        onChange={handleChange}
+                        required
+                        // className="border rounded p-1 mr-2 w-16"
+                        className="w-16 border-b-2 border-black outline-none text-center"
+                    />
+                    <label className="text-lg font-dragon">Strength</label>
                 </div>
-                <div>
-                    <label>
-                        Dexterity:
-                        <input
-                            type="number"
-                            name="saving_throws.dexterity"
-                            value={savingThrows.dexterity}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="saving_throws.dexterity_pro"
-                            checked={savingThrows.dexterity_pro}
-                            onChange={handleChange}
-                        />
-                        Proficient
-                    </label>
+                <div className="flex items-center mb-4">
+                    <input
+                        type="checkbox"
+                        name="saving_throws.dexterity_pro"
+                        checked={savingThrows.dexterity_pro}
+                        onChange={handleChange}
+                        // className="mr-2"
+                        className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-red-400 checked:border-black focus:ring-5 focus:outline-none transition-all duration-300"
+                    
+                    />
+                    <input
+                        type="number"
+                        name="saving_throws.dexterity"
+                        value={savingThrows.dexterity}
+                        onChange={handleChange}
+                        required
+                        // className="border rounded p-1 mr-2 w-16"
+                        className="w-16 border-b-2 border-black outline-none text-center"
+
+                    />
+                    <label className="text-lg font-dragon">Dexterity</label>
                 </div>
-                <div>
-                    <label>
-                        Constitution:
-                        <input
-                            type="number"
-                            name="saving_throws.constitution"
-                            value={savingThrows.constitution}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="saving_throws.constitution_pro"
-                            checked={savingThrows.constitution_pro}
-                            onChange={handleChange}
-                        />
-                        Proficient
-                    </label>
+                <div className="flex items-center mb-4">
+                    <input
+                        type="checkbox"
+                        name="saving_throws.constitution_pro"
+                        checked={savingThrows.constitution_pro}
+                        onChange={handleChange}
+                        // className="mr-2"
+                        className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-red-400 checked:border-black focus:ring-5 focus:outline-none transition-all duration-300"
+                    
+                    />
+                    <input
+                        type="number"
+                        name="saving_throws.constitution"
+                        value={savingThrows.constitution}
+                        onChange={handleChange}
+                        required
+                        // className="border rounded p-1 mr-2 w-16"
+                        className="w-16 border-b-2 border-black outline-none text-center"
+
+                    />
+                    <label className="text-lg font-dragon">Constitution</label>
                 </div>
-                <div>
-                    <label>
-                        Intelligence:
-                        <input
-                            type="number"
-                            name="saving_throws.intelligence"
-                            value={savingThrows.intelligence}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="saving_throws.intelligence_pro"
-                            checked={savingThrows.intelligence_pro}
-                            onChange={handleChange}
-                        />
-                        Proficient
-                    </label>
+                <div className="flex items-center mb-4">
+                    <input
+                        type="checkbox"
+                        name="saving_throws.intelligence_pro"
+                        checked={savingThrows.intelligence_pro}
+                        onChange={handleChange}
+                        // className="mr-2"
+                        className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-red-400 checked:border-black focus:ring-5 focus:outline-none transition-all duration-300"
+                    
+                    />
+                    <input
+                        type="number"
+                        name="saving_throws.intelligence"
+                        value={savingThrows.intelligence}
+                        onChange={handleChange}
+                        required
+                        // className="border rounded p-1 mr-2 w-16"
+                        className="w-16 border-b-2 border-black outline-none text-center"
+
+                    />
+                    <label className="text-lg font-dragon">Intelligence</label>
                 </div>
-                <div>
-                    <label>
-                        Wisdom:
-                        <input
-                            type="number"
-                            name="saving_throws.wisdom"
-                            value={savingThrows.wisdom}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="saving_throws.wisdom_pro"
-                            checked={savingThrows.wisdom_pro}
-                            onChange={handleChange}
-                        />
-                        Proficient
-                    </label>
+                <div className="flex items-center mb-4">
+                    <input
+                        type="checkbox"
+                        name="saving_throws.wisdom_pro"
+                        checked={savingThrows.wisdom_pro}
+                        onChange={handleChange}
+                        // className="mr-2"
+                        className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-red-400 checked:border-black focus:ring-5 focus:outline-none transition-all duration-300"
+                    
+                    />
+                    <input
+                        type="number"
+                        name="saving_throws.wisdom"
+                        value={savingThrows.wisdom}
+                        onChange={handleChange}
+                        required
+                        // className="border rounded p-1 mr-2 w-16"
+                        className="w-16 border-b-2 border-black outline-none text-center"
+
+                    />
+                    <label className="text-lg font-dragon">Wisdom</label>
                 </div>
-                <div>
-                    <label>
-                        Charisma:
-                        <input
-                            type="number"
-                            name="saving_throws.charisma"
-                            value={savingThrows.charisma}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="saving_throws.charisma_pro"
-                            checked={savingThrows.charisma_pro}
-                            onChange={handleChange}
-                        />
-                        Proficient
-                    </label>
+                <div className="flex items-center mb-4">
+                    <input
+                        type="checkbox"
+                        name="saving_throws.charisma_pro"
+                        checked={savingThrows.charisma_pro}
+                        onChange={handleChange}
+                        // className="mr-2"
+                        className="h-4 w-4 appearance-none rounded-full border-2 border-black checked:bg-red-400 checked:border-black focus:ring-5 focus:outline-none transition-all duration-300"
+                    
+                    />
+                    <input
+                        type="number"
+                        name="saving_throws.charisma"
+                        value={savingThrows.charisma}
+                        onChange={handleChange}
+                        required
+                        // className="border rounded p-1 mr-2 w-16"
+                        className="w-16 border-b-2 border-black outline-none text-center"
+
+                    />
+                    <label className="text-lg font-dragon">Charisma</label>
                 </div>
-                
             </form>
         </div>
     );
 }
 
 export default CharacterSaving;
-
