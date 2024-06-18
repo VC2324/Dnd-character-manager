ffbda436a960837c6d11a30eb5bce56f5b5f42cd
Victor <victorac2324@gmail.com>
2024-06-18 15:15:31 -0400

    added some css to chracter sheet

--- client/src/components/CharacterProfile.jsx
+++ client/src/components/CharacterProfile.jsx
@@ -29,6 +29,100 @@
 
 // export default CharacterProfile;
 
+
+// import React from 'react';
+
+// function CharacterProfile({ handleSubmit, handleChange, profData }) {
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <div className="grid grid-cols-3 gap-4 mb-4">
+//                 <div className="col-span-1">
+//                     <label className="block mb-1 underline">Name:</label>
+//                     <input
+//                         type="text"
+//                         name="name"
+//                         value={profData.name}
+//                         onChange={handleChange}
+//                         // className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+//                         required
+//                     />
+//                 </div>
+//                 <div className="col-span-1">
+//                     <label className="block mb-1 underline">Class:</label>
+//                     <input
+//                         type="text"
+//                         name="klass"
+//                         value={profData.klass}
+//                         onChange={handleChange}
+//                         className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+//                         required
+//                     />
+//                 </div>
+//                 <div className="col-span-1">
+//                     <label className="block mb-1 underline">Level:</label>
+//                     <input
+//                         type="number"
+//                         name="level"
+//                         value={profData.level}
+//                         onChange={handleChange}
+//                         className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+//                         required
+//                     />
+//                 </div>
+//                 <div className="col-span-1">
+//                     <label className="block mb-1 underline">Background:</label>
+//                     <input
+//                         type="text"
+//                         name="background"
+//                         value={profData.background}
+//                         onChange={handleChange}
+//                         className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+//                         required
+//                     />
+//                 </div>
+//                 <div className="col-span-1">
+//                     <label className="block mb-1 underline">Race:</label>
+//                     <input
+//                         type="text"
+//                         name="race"
+//                         value={profData.race}
+//                         onChange={handleChange}
+//                         className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+//                         required
+//                     />
+//                 </div>
+//                 <div className="col-span-1">
+//                     <label className="block mb-1 underline">XP:</label>
+//                     <input
+//                         type="number"
+//                         name="xp"
+//                         value={profData.xp}
+//                         onChange={handleChange}
+//                         className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+//                         required
+//                     />
+//                 </div>
+//                 <div className="col-span-1">
+//                     <label className="block mb-1 underline">Alignment:</label>
+//                     <input
+//                         type="text"
+//                         name="alignment"
+//                         value={profData.alignment}
+//                         onChange={handleChange}
+//                         // className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+//                         required
+//                     />
+//                 </div>
+//             </div>
+//         </form>
+//     );
+// }
+
+// export default CharacterProfile;
+
+
+
+
 import React from 'react';
 
 function CharacterProfile({ handleSubmit, handleChange, profData }) {
@@ -36,79 +130,79 @@ function CharacterProfile({ handleSubmit, handleChange, profData }) {
         <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-3 gap-4 mb-4">
                 <div className="col-span-1">
-                    <label className="block mb-1 underline">Name:</label>
+                    <label className="block mb-1  font-dragon underline">Name:</label>
                     <input
                         type="text"
                         name="name"
                         value={profData.name}
                         onChange={handleChange}
-                        // className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none shadow-lg"
                         required
                     />
                 </div>
                 <div className="col-span-1">
-                    <label className="block mb-1 underline">Class:</label>
+                    <label className="block mb-1 underline font-dragon ">Class:</label>
                     <input
                         type="text"
                         name="klass"
                         value={profData.klass}
                         onChange={handleChange}
-                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none shadow-lg"
                         required
                     />
                 </div>
                 <div className="col-span-1">
-                    <label className="block mb-1 underline">Level:</label>
+                    <label className="block mb-1 font-dragon underline">Level:</label>
                     <input
                         type="number"
                         name="level"
                         value={profData.level}
                         onChange={handleChange}
-                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+                        className="block w-full border-b  border-gray-400 focus:border-blue-500 outline-none shadow-lg"
                         required
                     />
                 </div>
                 <div className="col-span-1">
-                    <label className="block mb-1 underline">Background:</label>
+                    <label className="block mb-1 underline font-dragon">Background:</label>
                     <input
                         type="text"
                         name="background"
                         value={profData.background}
                         onChange={handleChange}
-                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none shadow-lg"
                         required
                     />
                 </div>
                 <div className="col-span-1">
-                    <label className="block mb-1 underline">Race:</label>
+                    <label className="block mb-1 font-dragon underline">Race:</label>
                     <input
                         type="text"
                         name="race"
                         value={profData.race}
                         onChange={handleChange}
-                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none shadow-lg"
                         required
                     />
                 </div>
                 <div className="col-span-1">
-                    <label className="block mb-1 underline">XP:</label>
+                    <label className="block mb-1 underline font-dragon" >XP:</label>
                     <input
                         type="number"
                         name="xp"
                         value={profData.xp}
                         onChange={handleChange}
-                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none shadow-lg"
                         required
                     />
                 </div>
                 <div className="col-span-1">
-                    <label className="block mb-1 underline">Alignment:</label>
+                    <label className="block mb-1 underline font-dragon">Alignment:</label>
                     <input
                         type="text"
                         name="alignment"
                         value={profData.alignment}
                         onChange={handleChange}
-                        // className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none"
+                        className="block w-full border-b border-gray-400 focus:border-blue-500 outline-none shadow-lg"
                         required
                     />
                 </div>
@@ -118,5 +212,3 @@ function CharacterProfile({ handleSubmit, handleChange, profData }) {
 }
 
 export default CharacterProfile;
-
-
--- client/src/components/CharacterStats.jsx
+++ client/src/components/CharacterStats.jsx
@@ -1,50 +1,242 @@
 
+import React from 'react';
+
 function CharacterStats({ handleChange, stats }) {
     return (
-        <div>
-            <div>
-                <label>Strength Mod:<input type="number" name="strength_mod" value={stats.strength_mod} onChange={handleChange} required /></label>
-                <div>
-                    <label>Strength:<input type="number" name="strength" value={stats.strength} onChange={handleChange} required /></label>
+        <form className="w-full max-w-md p-4">
+            <div className="flex flex-col gap-4">
+                {/* Strength */}
+                        <label className="block  underline font-dragon">Strength:</label>
+              
+                <div className="border border-gray-300 rounded-lg shadow-md p-4 outline-double w-1/4">
+
+                    <div className="flex flex-col mb-2">
+                        <input
+                            type="number"
+                            name="strength"
+                            value={stats.strength}
+                            onChange={handleChange}
+                            className="block w-12 p-2 rounded shadow-sm outline-none"
+                            required
+                        />
+                    </div>
+                    <div >
+                        <label className="block mb-1 underline font-dragon">Modifier:</label>
+                        <input
+                            type="number"
+                            name="strength_mod"
+                            value={stats.strength_mod}
+                            onChange={handleChange}
+                            className="block w-16 p-2 rounded"
+                            style={{
+                                border: '5px solid #000',
+                                borderRadius: '0px 0px 40px 40px',
+                                outline: 'none',
+                                textAlign: 'center', // Center-align the input value
+                            }}
+                            required
+                        />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label>Dexterity Mod:<input type="number" name="dexterity_mod" value={stats.dexterity_mod} onChange={handleChange} required /></label>
-                <div>
-                    <label>Dexterity:<input type="number" name="dexterity" value={stats.dexterity} onChange={handleChange} required /></label>
+
+                {/* Dexterity */}
+                        <label className="block mb-1 underline font-dragon">Dexterity:</label>
+                {/* <div className="border border-gray-300 rounded-lg shadow-md p-4"> */}
+                <div className="border border-gray-300 rounded-lg shadow-md p-4 outline-double w-1/4">
+
+                    <div className="flex flex-col mb-2">
+                        <input
+                            type="number"
+                            name="dexterity"
+                            value={stats.dexterity}
+                            onChange={handleChange}
+                            className="block w-12 p-2 rounded border focus:border-blue-500 outline-none"
+                            required
+                        />
+                    </div>
+                    <div >
+                        <label className="block mb-1 underline font-dragon">Modifier:</label>
+                        <input
+                            type="number"
+                            name="dexterity_mod"
+                            value={stats.dexterity_mod}
+                            onChange={handleChange}
+                            className="block w-16 p-2 rounded"
+                            style={{
+                                border: '5px solid #000',
+                                borderRadius: '0px 0px 40px 40px',
+                                outline: 'none',
+                                textAlign: 'center', // Center-align the input value
+                            }}
+                            required
+                        />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label>Constitution Mod:<input type="number" name="constitution_mod" value={stats.constitution_mod} onChange={handleChange} required /></label>
-                <div>
-                    <label>Constitution:<input type="number" name="constitution" value={stats.constitution} onChange={handleChange} required /></label>
+
+                {/* Constitution */}
+                        <label className="block mb-1 underline font-dragon">Constitution:</label>
+                        <div className="border border-gray-300 rounded-lg shadow-md p-4 outline-double w-1/4">
+
+                    <div className="flex flex-col mb-2">
+                        <input
+                            type="number"
+                            name="constitution"
+                            value={stats.constitution}
+                            onChange={handleChange}
+                            className="block w-12 p-2 rounded border focus:border-blue-500 outline-none"
+                            required
+                        />
+                    </div>
+                    <div >
+                        <label className="block mb-1 underline font-dragon">Modifier:</label>
+                        <input
+                            type="number"
+                            name="constitution_mod"
+                            value={stats.constitution_mod}
+                            onChange={handleChange}
+                            className="block w-16 p-2 rounded"
+                            style={{
+                                border: '5px solid #000',
+                                borderRadius: '0px 0px 40px 40px',
+                                outline: 'none',
+                                textAlign: 'center', // Center-align the input value
+                            }}
+                            required
+                        />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label>Intelligence Mod:<input type="number" name="intelligence_mod" value={stats.intelligence_mod} onChange={handleChange} required /></label>
-                <div>
-                    <label>Intelligence:<input type="number" name="intelligence" value={stats.intelligence} onChange={handleChange} required /></label>
+
+                {/* Intelligence */}
+                        <label className="block mb-1 underline font-dragon">Intelligence:</label>
+                <div className="border border-gray-300 rounded-lg shadow-md p-4 outline-double w-1/4">
+                            
+                    <div className="flex flex-col mb-2">
+                        <input
+                            type="number"
+                            name="intelligence"
+                            value={stats.intelligence}
+                            onChange={handleChange}
+                            className="block w-12 p-2 rounded border focus:border-blue-500 outline-none"
+                            required
+                        />
+                    </div>
+                    <div >
+                        <label className="block mb-1 underline font-dragon">Modifier:</label>
+                        <input
+                            type="number"
+                            name="intelligence_mod"
+                            value={stats.intelligence_mod}
+                            onChange={handleChange}
+                            className="block w-16 p-2 rounded"
+                            style={{
+                                border: '5px solid #000',
+                                borderRadius: '0px 0px 40px 40px',
+                                outline: 'none',
+                                textAlign: 'center', // Center-align the input value
+                            }}
+                            required
+                        />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label>Wisdom Mod:<input type="number" name="wisdom_mod" value={stats.wisdom_mod} onChange={handleChange} required /></label>
-                <div>
-                    <label>Wisdom:<input type="number" name="wisdom" value={stats.wisdom} onChange={handleChange} required /></label>
+
+                {/* Wisdom */}
+                        <label className="block mb-1 underline font-dragon">Wisdom:</label>
+                        <div className="border border-gray-300 rounded-lg shadow-md p-4 outline-double w-1/4">
+                    <div className="flex flex-col mb-2">
+                        <input
+                            type="number"
+                            name="wisdom"
+                            value={stats.wisdom}
+                            onChange={handleChange}
+                            className="block w-12 p-2 rounded border focus:border-blue-500 outline-none"
+                            required
+                        />
+                    </div>
+                    <div >
+                        <label className="block mb-1 underline font-dragon">Modifier:</label>
+                        <input
+                            type="number"
+                            name="wisdom_mod"
+                            value={stats.wisdom_mod}
+                            onChange={handleChange}
+                            className="block w-16 p-2 rounded"
+                            style={{
+                                border: '5px solid #000',
+                                borderRadius: '0px 0px 40px 40px',
+                                outline: 'none',
+                                textAlign: 'center', // Center-align the input value
+                            }}
+                            required
+                        />
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label>Charisma Mod:<input type="number" name="charisma_mod" value={stats.charisma_mod} onChange={handleChange} required /></label>
-                <div>
-                    <label>Charisma:<input type="number" name="charisma" value={stats.charisma} onChange={handleChange} required /></label>
+
+                {/* Charisma */}
+                        <label className="block mb-1 underline font-dragon">Charisma:</label>
+                        <div className="border border-gray-300 rounded-lg shadow-md p-4 outline-double w-1/4">
+                    <div className="flex flex-col mb-2">
+                        <input
+                            type="number"
+                            name="charisma"
+                            value={stats.charisma}
+                            onChange={handleChange}
+                            className="block w-12 p-2 rounded border focus:border-blue-500 outline-none"
+                            required
+                        />
+                    </div>
+                    <div >
+                        <label className="block mb-1 underline font-dragon">Modifier:</label>
+                        <input
+                            type="number"
+                            name="charisma_mod"
+                            value={stats.charisma_mod}
+                            onChange={handleChange}
+                            className="block w-16 p-2 rounded"
+                            style={{
+                                border: '5px solid #000',
+                                borderRadius: '0px 0px 40px 40px',
+                                outline: 'none',
+                                textAlign: 'center', // Center-align the input value
+                            }}
+                            required
+                        />
+                    </div>
                 </div>
+
+                {/* Wisdom Perception */}
+                        <label className="block mb-1 underline font-dragon">Wisdom Perception:</label>
+                        <div className="border border-gray-300 rounded-lg shadow-md p-4 outline-double w-1/4">
+                    <div className="flex flex-col mb-2">
+                        <input
+                            type="number"
+                            name="wis_perception"
+                            value={stats.wis_perception}
+                            onChange={handleChange}
+                            className="block w-12 p-2 rounded border focus:border-blue-500 outline-none"
+                            required
+                        />
+                    </div>
+                    <div >
+                        <label className="block mb-1 underline font-dragon">Modifier:</label>
+                        <input
+                            type="number"
+                            name="wis_perception_mod"
+                            value={stats.wis_perception_mod}
+                            onChange={handleChange}
+                            className="block w-16 p-2 rounded"
+                            style={{
+                                border: '5px solid #000',
+                                borderRadius: '0px 0px 40px 40px',
+                                outline: 'none',
+                                textAlign: 'center', // Center-align the input value
+                            }}
+                            required
+                        />
+                    </div>
+                </div>
+
             </div>
-            <div>
-                <label>Wisdom Perception:<input type="number" name="wis_perception" value={stats.wis_perception} onChange={handleChange} required /></label>
-            </div>
-            <div>
-                <label>Wisdom Perception Mod:<input type="number" name="wis_perception_mod" value={stats.wis_perception_mod} onChange={handleChange} required /></label>
-            </div>
-        </div>
+        </form>
     );
 }
 
